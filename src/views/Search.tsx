8053f8d24ace34b2b82d66934ec96e6fe1e01c4b
almin <fir512@126.com>
2021-04-27 10:17:40 +0800

    fix: 下拉刷新，滚动加载结合 BScroll demo

--- src/views/Search.tsx
+++ src/views/Search.tsx
@@ -1,12 +1,15 @@
-import { computed, defineComponent, onMounted, reactive, watch } from "vue"
+import { computed, defineComponent, onMounted, reactive, watch, ref, nextTick } from "vue"
 import { useStoreHooks } from '@/hooks';
 import { useRouter, useRoute } from "vue-router"
 import "@/assets/sass/search.scss"
 import OneBusiness from "@/components/OneBusiness.vue"
+import BScroll from "better-scroll"
+import { falseBussinessbrief } from "@/falsedata/falsedata"
 interface search {
   search_text: string,
   search_res: any[],
-  keyword: string
+  keyword: string,
+  [x: string]: any
 }
 export default defineComponent({
   name: "Search",
@@ -18,8 +21,12 @@ export default defineComponent({
     const route = useRoute();
     const state: search = reactive({
       search_text: "",
-      search_res: [],
-      keyword: route?.params?.keyword.toString()
+      search_res: falseBussinessbrief,
+      keyword: route?.params?.keyword.toString(),
+      scroll: "",
+      pageNum: 1,
+      pageSize: 20,
+      isMore: true
     })
     const back = () => {
       router.go(-1)
@@ -30,7 +37,11 @@ export default defineComponent({
     })
     const getFalseBussinessbrief = computed(() => getters['getFalseBussinessbrief'])
     onMounted(() => {
-      sameMethod(state.keyword)
+      sameMethod(state.keyword);
+      nextTick(() => {
+        _initScroll();
+        getList()
+      })
     })
     const sameMethod = (text: string) => {
       const data = getFalseBussinessbrief.value;
@@ -40,11 +51,51 @@ export default defineComponent({
         }
       }
     }
+    const _initScroll = () => {
+      state.scroll = new BScroll("#searchContent", {
+        scrollY: true,
+        click: true,
+        pullUpLoad: true,
+        pullDownRefresh: {
+          threshold: 50,
+          stop: 0
+        }
+      })
+      state.scroll.on("scroll", scrollHandler);
+      state.scroll.on("pullingDown", pullingDownHandler);
+      state.scroll.on("pullingUp", pullingUpHandler)
+    }
+    const scrollHandler = (pos: { y: number; }) => {
+      if (pos.y > 50) console.log("松开手加载")
+      else console.log("下拉刷新")
+    }
+    const pullingDownHandler = () => {
+      state.search_res = [];
+      state.pageNum = 1;
+      state.isMore = true;
+      getList();
+      state.scroll.finishPullDown();
+      state.scroll.refresh();
+    }
+    const pullingUpHandler = () => {
+      if (!state.isMore) {
+        state.scroll.finishPullUp();
+        return;
+      }
+      state.pageNum++;
+      getList();
+      state.scroll.finishPullUp();
+      state.scroll.refresh();
+    }
+    const getList = () => {
+      state.search_res = [...state.search_res, ...getFalseBussinessbrief.value];
+    }
     const htmlEl = () => {
       return state.search_res.map((item: any) => {
         return <OneBusiness item={item}></OneBusiness>
       })
     }
+
     return () => (
       <>
         <div class='search_wrap'>
@@ -54,7 +105,11 @@ export default defineComponent({
             </svg>
             <input type="search" v-model={state.search_text} class="v-md" placeholder="请输入商品名称" />
           </div>
-          {htmlEl()}
+          <div class="search-content" id="searchContent">
+            <div class="search_scroll">
+              {htmlEl()}
+            </div>
+          </div>
         </div>
       </>
     )
